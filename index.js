edfb9a16704081ed7a985b846ee52329cc0517f4
Ewan Harris <ewanharris93@gmail.com>
2018-07-13 20:47:27 +0100

    Use arguments too

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
+#!/usr/bin/env node
 const fetch = require('node-fetch');
 
 const { CLI } = require('cli-kit');
 const cli = new CLI({
 	help: true,
 	name: 'simple-cli',
-	options: {
-		'-p, --package [value]': {
-			desc: 'the package to query',
+	args: [
+		{
+			name: '<name>',
+			desc: 'The package name.',
 			type: 'string'
+		}
+	],
+	options: {
+		'--json': {
+			desc: 'output json',
+			
 		} 
 	}
 });
 
 cli
 	.exec()
-	.then(async ( {argv, _}) => {
-		const { package } = argv;
-		const res = await fetch(`https://registry.npmjs.com/${package}`);
+	.then(async ({ argv, _ }) => {
+		const { name, json } = argv;
+		const res = await fetch(`https://registry.npmjs.com/${name}`);
 		const data = await res.json();
-		const { latest } = data['dist-tags'];
-		const { time } = data;
-		console.log(`You asked for ${package}, the latest version is ${latest}, published on ${time[latest]}`);
+		if (data.error) {
+			throw new Error(`${name} doesn't exist!`);
+		}
+		if (json) {
+			console.log(data)
+		} else {
+			const { latest } = data['dist-tags'];
+			const { time } = data;
+			console.log(`You asked for ${name}, the latest version is ${latest}, published on ${time[latest]}`);
+		}
 	})
 	.catch(err => {
-		console.log(err);
+		console.log(err.message);
 	}) 
